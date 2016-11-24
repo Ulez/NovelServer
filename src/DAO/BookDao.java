a040bdb0a7ba1a9d9e90058944c1035546a22c80
Ulez <1104128773@qq.com>
2016-11-24 17:12:02 +0800

    .

--- src/DAO/BookDao.java
+++ src/DAO/BookDao.java
@@ -7,26 +7,36 @@ import java.sql.*;
  */
 public class BookDao {
     private Connection connection;
-    private String tableName = "book";
+    private String tableBook = "book";
+    private String tableChapter = "chapter";
 
-    public BookDao(){
+    public BookDao() {
 
     }
 
-    public int insertBook(int id, String book_name, String book_url) throws Exception {
+    public int insertBook(String book_name, String book_url) throws Exception {
         if (connection == null)
             connection = MysqlTool.getmInstance().getConnection();
-        String query = " insert into " + tableName + " (book_id, book_name, book_url)"
-                + " values (?, ?, ?)";
-        // create the mysql insert preparedstatement
+        String query = " insert into " + tableBook + " (book_id, book_name, book_url)" + " values (?, ?, ?)";
         PreparedStatement preparedStmt = connection.prepareStatement(query);
-        preparedStmt.setInt(1, id);
-        preparedStmt.setString(2, book_name);
-        preparedStmt.setString(3, book_url);
-        int rt=preparedStmt.executeUpdate();
+        preparedStmt.setString(3, book_name);
+        preparedStmt.setString(4, book_url);
+        int rt = preparedStmt.executeUpdate();
         connection.close();
-        /** Closing JDBC connection */
-//        rs.close();
+        preparedStmt.close();
+        connection.close();
+        return rt;
+    }
+
+    public int insertChapter(String chapter_name, String chapter_url) throws Exception {
+        if (connection == null)
+            connection = MysqlTool.getmInstance().getConnection();
+        String query = " insert into " + tableChapter + " (chapter_id, chapter_name, chapter_url)" + " values (?, ?, ?)";
+        PreparedStatement preparedStmt = connection.prepareStatement(query);
+        preparedStmt.setString(3, chapter_name);
+        preparedStmt.setString(4, chapter_url);
+        int rt = preparedStmt.executeUpdate();
+        connection.close();
         preparedStmt.close();
         connection.close();
         return rt;
